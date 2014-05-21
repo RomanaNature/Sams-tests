f55c7ea46e717bbeccea9b53d64f96eecff172de
unknown <r.sheikh@NPGNYMXL31219CV.mpl.root-domain.org>
2014-05-21 15:22:41 -0400

    First Commit

---
+++ Selenium/SA digital single page.java
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="selenium.base" href="http://www.yopmail.com/" />
+<title>SA digital single page</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">SA digital single page</td></tr>
+</thead><tbody>
+<tr>
+	<td>open</td>
+	<td>/en/</td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=login</td>
+	<td>Romana100</td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>css=input.sbut</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>//div[@id='SAD']/div/p/img[2]</td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=firstName</td>
+	<td>Romana</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=lastName</td>
+	<td>Sheikh</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=emailAddress</td>
+	<td>romana100@yopmail.com</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=emailAddressConfirm</td>
+	<td>romana100@yopmail.com</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=screenName</td>
+	<td>Romana209</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=password</td>
+	<td>nature123</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=passwordConfirm</td>
+	<td>nature123</td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>id=List_OptIn_External1</td>
+	<td></td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>id=acceptedTC</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>id=submitCreateAccount</td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=name</td>
+	<td>Romana Sheikh</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=shippingStreetAddress</td>
+	<td>167 34th st</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=shippingCity</td>
+	<td>Ozone park</td>
+</tr>
+<tr>
+	<td>select</td>
+	<td>id=stateCode</td>
+	<td>label=New York</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=shippingPostalCode</td>
+	<td>11416</td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>id=submitFulfillment</td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=nameOnCreditCard</td>
+	<td>Romana Sheikh</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=creditCardNumber</td>
+	<td>4112344112344113</td>
+</tr>
+<tr>
+	<td>select</td>
+	<td>id=ccMonth</td>
+	<td>label=12</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=securityCode</td>
+	<td>411</td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>id=submitBilling</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>//button[@type='submit']</td>
+	<td></td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>css=span.slientext</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>link=Order History</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>link=View Order Details</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>link=Scientific American Digital Subscription</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>css=a.cover &gt; img</td>
+	<td></td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>link=Download Full Issue PDF</td>
+	<td></td>
+</tr>
+
+</tbody></table>
+</body>
+</html>
