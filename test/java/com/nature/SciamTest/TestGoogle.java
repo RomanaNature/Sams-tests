56fa8a73b9bfdefc1c7ec6e70556a5df89b902ea
unknown <r.sheikh@NPGNYMXL31219CV.mpl.root-domain.org>
2014-09-15 14:04:38 -0400

    TestGoogle

---
+++ test/java/com/nature/SciamTest/TestGoogle.java
@@ -0,0 +1,38 @@
+package com.nature.SciamTest;
+
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestGoogle {
+  private WebDriver driver;
+  private String baseUrl;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitled() throws Exception {
+    driver.get(baseUrl + "/?gws_rd=ssl");
+    driver.findElement(By.id("gbqfq")).clear();
+    driver.findElement(By.id("gbqfq")).sendKeys("flower images");
+    driver.findElement(By.name("imgthumb1")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
