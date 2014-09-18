d6486c64b12755bf939b63f72671dc829903c73a
RomanaNature <onju90@yahoo.com>
2014-09-18 16:13:54 -0400

    Update PurchaseTest.java

--- src/test/java/com/nature/SciamTest/PurchaseTest.java
+++ src/test/java/com/nature/SciamTest/PurchaseTest.java
@@ -6,7 +6,6 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.Select;
 
 public class PurchaseTest {
@@ -17,6 +16,7 @@ public class PurchaseTest {
 	@Before
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
+		System.setProperty("webdriver.firefox.bin","/usr/bin/Firefox.exe");
 		// baseUrl = "http://test-www.scientificamerican.com/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
@@ -26,8 +26,8 @@ public class PurchaseTest {
 		// driver.get(baseUrl +
 		// "/store/subscribe/scientific-american-digital/?responseKey=");
 		driver.manage().window().maximize();
-		FirefoxProfile profile = new FirefoxProfile();
-		profile.setPreference("network.http.phishy-userpass-length", 255);
+		//FirefoxProfile profile = new FirefoxProfile();
+		//profile.setPreference("network.http.phishy-userpass-length", 255);
 		driver.get("https://web:guest@test-www.scientificamerican.com/store/subscribe/scientific-american-digital/?responseKey=");
 		Assert.assertEquals("Not the proper status code!", 200, Integer.parseInt(driver.findElement(By.id("web_response")).getAttribute("content")));
 		Thread.sleep(1000);
